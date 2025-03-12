698d792bba39d7cf77ff3b7f38d3ae991bfc2ab6
Billapink <sibyllensheikh@outlook.com>
2025-03-12 10:47:33 +0000

    Created the skeleton components for the welcome, login and sign up pages.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,12 +8,15 @@ function App() {
     <div className="bg-gray-800 text-white min-h-screen items-center justify-center">
       <h1 className="p-6 max-w-md mx-auto text-4xl font-bold ">Billio</h1>
       <nav>
-          <Link to="/">Task List</Link> | <Link to="/chat">Chat</Link> | <Link to="/home">Home</Link>
+      <Link to="/welcome">Welcome</Link> | <Link to="/home">Home</Link> | <Link to="/signup">Sign Up</Link> | <Link to="/login">Log In</Link> | <Link to="/">Task List</Link> | <Link to="/chat">Chat</Link> 
       </nav>
       <Routes>
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/tasklist" element={<TaskList />} />
           <Route path="/chat" element={<Chat />} />
-          <Route path="/home" element={<Home />} />
       </Routes>
   </div>
   );
