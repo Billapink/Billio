64faf77ae00eda02c7bd8447b19e572cd4942d2b
Billapink <sibyllensheikh@outlook.com>
2025-03-14 22:05:07 +0000

    added tasklist link to nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="bg-gray-800 text-white min-h-screen items-center justify-center">
       <h1 className="p-6 max-w-md mx-auto text-4xl font-bold ">Billio</h1>
       <nav>
-      <Link to="/welcome">Welcome</Link> | <Link to="/home">Home</Link> | <Link to="/signup">Sign Up</Link> | <Link to="/login">Log In</Link> | <Link to="/">Task List</Link> | <Link to="/chat">Chat</Link> 
+      <Link to="/welcome">Welcome</Link> | <Link to="/home">Home</Link> | <Link to="/signup">Sign Up</Link> | <Link to="/login">Log In</Link> | <Link to="/tasklist">Task List</Link> | <Link to="/chat">Chat</Link> 
       </nav>
       <Routes>
           <Route path="/welcome" element={<Welcome />} />
