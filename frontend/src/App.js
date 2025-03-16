cdddcc969a9ffe3272dbab5dfd7410c93954bd7a
Billapink <sibyllensheikh@outlook.com>
2025-03-16 22:14:30 +0000

    centring the text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import Home from "./components/Home";
 function App() {
   return (
     <div className="bg-gray-800 text-white min-h-screen items-center justify-center">
-      <h1 className="p-6 max-w-md mx-auto text-centre text-4xl font-bold ">Billio</h1>
+      <h1 className="p-6 max-w-md mx-auto justify-centre text-4xl font-bold ">Billio</h1>
       <nav>
       <Link to="/welcome">Welcome</Link> | <Link to="/home">Home</Link> | <Link to="/signup">Sign Up</Link> | <Link to="/login">Log In</Link> | <Link to="/tasklist">Task List</Link> | <Link to="/chat">Chat</Link> 
       </nav>
