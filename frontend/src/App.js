d72cf9b535a028c866eaa3200d3dd29eaac4033f
Billapink <sibyllensheikh@outlook.com>
2025-03-16 22:17:54 +0000

    correcting the parent container that has flex attribute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,8 @@ import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="flex bg-gray-800 text-white min-h-screen items-center justify-center">
+    <div className="bg-gray-800 text-white min-h-screen items-center justify-center">
+      <div className="flex" ></div>
       <h1 className="p-6 max-w-md mx-auto justify-centre text-4xl font-bold ">Billio</h1>
       <nav>
       <Link to="/welcome">Welcome</Link> | <Link to="/home">Home</Link> | <Link to="/signup">Sign Up</Link> | <Link to="/login">Log In</Link> | <Link to="/tasklist">Task List</Link> | <Link to="/chat">Chat</Link> 
