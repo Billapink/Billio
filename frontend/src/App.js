0e966ea3082fa4acc74da3f66fd224019d34bf3b
Billapink <sibyllensheikh@outlook.com>
2025-03-16 22:33:10 +0000

    adjusting header styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,9 @@ import Home from "./components/Home";
 function App() {
   return (
     <div className="bg-white text-white min-h-screen items-center justify-center">
-      <h1 className="p-6 max-w-md mx-auto text-4xl text-purple-500 font-bold justify-centre">Billio</h1>
+      <div className="max-w-md mx-auto flex">
+      <h1 className="p-6  text-4xl text-purple-500 font-bold justify-centre">Billio</h1>
+      </div>
       <nav>
       <Link to="/welcome">Welcome</Link> | <Link to="/home">Home</Link> | <Link to="/signup">Sign Up</Link> | <Link to="/login">Log In</Link> | <Link to="/tasklist">Task List</Link> | <Link to="/chat">Chat</Link> 
       </nav>
