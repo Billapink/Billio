b1dc216d6ef46c8d1f425fc180c66a64486f0d49
Billapink <sibyllensheikh@outlook.com>
2025-03-17 09:16:20 +0000

    changing whole nav to black

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,8 +14,8 @@ function App() {
       <div className="max-w-md mx-auto justify-centre">
       <h1 className="p-6  text-4xl text-purple-500 font-bold justify-centre">Billio</h1>
       </div>
-      <nav className="text-black">
-      <Link   to="/welcome">Welcome</Link> | <Link to="/home">Home</Link> | <Link to="/signup">Sign Up</Link> | <Link to="/login">Log In</Link> | <Link to="/tasklist">Task List</Link> | <Link to="/chat">Chat</Link> 
+      <nav>
+      <Link   to="/welcome">Welcome</Link> | <Link className="text-black" to="/home">Home</Link> | <Link className="text-black" to="/signup">Sign Up</Link> | <Link className="text-black" to="/login">Log In</Link> | <Link className="text-black" to="/tasklist">Task List</Link> | <Link className="text-black" to="/chat">Chat</Link> 
       </nav>
       <Routes>
           <Route path="/welcome" element={<Welcome />} />
