42da7c321b368b16c553142bec04571833145f86
Billapink <sibyllensheikh@outlook.com>
2025-03-31 15:07:49 +0100

    Adding a navbar to the frontpage.

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import TaskForm from './TaskForm';
 import {useNavigate} from 'react-router-dom';
 import billioLogoFront from '/Users/sadaf/Desktop/GitHubProjects/Billio/Billio/frontend/src/assets/logos/billio-front.png';
+import NavBar from './NavBar';
 
 function Welcome() {
     const [tasks, setTasks] = useState([]);
@@ -41,6 +42,7 @@ function Welcome() {
                 </div>
                 <button className="p-3 px-20 bg-purple-500 text-white font-semibold rounded-full" type="button" onClick={()=> handleNavigation('/login')} >Log In</button>
                 <button className="p-3 px-20 bg-purple-500 text-white font-semibold rounded-full" type="button" onClick={()=> handleNavigation('/signup')} >Sign Up</button>
+                <NavBar/>
                 </div>
             </div>
             
