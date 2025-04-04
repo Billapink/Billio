05cde0c931ded30fc9486c5524f6b97b1e2b314f
Billapink <sibyllensheikh@outlook.com>
2025-04-04 01:12:30 +0100

    Making the logo of the header a button.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,20 @@
 import {React} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 function Header() {
 
+    const navigate = useNavigate();
+
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
 
     return (
         <div>
         <div className='fixed top-0 left-0 right-0 shadow-md'>
+        <button onClick={handleNavigation('/home')} >
         <img className='m-5 w-[70px]' src= '/images/billio-header.png'/>
+        </button>
         </div>
         </div>
     );
